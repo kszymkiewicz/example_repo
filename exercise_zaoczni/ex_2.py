c357c9f2e37c0fce116a36965c4cd7714be202b7
Kamil Szymkiewicz <64159.ue.poznan.pl>
2023-04-16 17:34:29 +0200

    Dodany romb

--- exercise_zaoczni/ex_2.py
+++ exercise_zaoczni/ex_2.py
@@ -9,3 +9,15 @@ pole = 1/2 * a * h
 obwod = a + b + c
 
 print('Pole trojkata wynosi ' + str(pole) + ', a obwod wynosi '+ str(obwod))
+
+#romb
+
+a_romb = 10
+h_romb = 13
+
+pole_romb = a_romb * h_romb
+obwod_romb = 4 * a_romb
+
+print('Pole rombu wynosi '+ str(pole_romb) + ', a obowod wynosi ' + str(obwod_romb))
+
+
