435e87765dca315250905dcb75e639aef5cb0f56
Kamil Szymkiewicz <64159.ue.poznan.pl>
2023-05-28 13:55:30 +0200

    zadanie 1.1

---
+++ exercise_zaoczni/ex_3.py
@@ -0,0 +1,9 @@
+# zadanie 1.1
+
+hello = "Hello"
+student = "Ola"
+
+# oczekiwany rezultat: Hello Ola
+# wykorzystaj w princie zmienne hello i student
+print("Hello Kasia")
+print ("{} {}".format(hello,student))
