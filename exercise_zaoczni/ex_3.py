49b89ecd5d180ac4b8a617ebcf73f7e60883c430
Kamil Szymkiewicz <64159.ue.poznan.pl>
2023-05-28 14:28:24 +0200

    zadanie 1.8

--- exercise_zaoczni/ex_3.py
+++ exercise_zaoczni/ex_3.py
@@ -78,3 +78,19 @@ studenci.sort()
 print("Alfabetyczna lista studentow wynosi: ")
 for student in studenci:
     print(student)
+
+
+# zadanie 1.8
+
+# posortuj alfabetycznie (od nazwiska) studentow
+studenci = ["Anna Szczesny", "Tomasz Nijaki", "Barbara Kowalska", "Jan Niezbedny"]
+studenci = sorted(studenci, key=lambda x: x[1])
+
+# oczekiwany rezultat:
+# Barbara Kowalska
+# Jan Niezbedny
+# Tomasz Nijaki
+# Anna Szczesny
+print("Alfabetyczna lista studentow wynosi: ")
+for student in studenci:
+    print(student)
