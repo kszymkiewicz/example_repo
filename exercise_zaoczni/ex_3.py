27eab3cd707268ff0b1040b82d9eec6ff457ec36
Kamil Szymkiewicz <64159.ue.poznan.pl>
2023-05-28 14:09:30 +0200

    zadanie 1.6

--- exercise_zaoczni/ex_3.py
+++ exercise_zaoczni/ex_3.py
@@ -50,3 +50,15 @@ wynik = liczba**potega
 # oczekiwany rezultat:
 # Wynik wynosi: 81
 print("Wynik wynosi: {}".format(wynik))
+
+
+# zadanie 1.6
+
+# policz ilosc nawiasow ( w danym ciagu znakow
+
+ciag_znakow = "edbw(hdakqas(skqskahb))adwndwb(wgwidn()dsqwhjdw)"
+liczba_nawiasow_otwierajacych = ciag_znakow.count("(")
+
+# oczekiwany rezultat:
+# Liczba nawiasow otwierajacych wynosi: 4
+print("Liczba nawiasow otwierajacych wynosi: " + str(liczba_nawiasow_otwierajacych))
